8b93d434eac827d8bccebb8bd3e914ab00529f00
Navneet Nagpal <navneetnagpal84@gmail.com>
2022-01-18 20:26:30 +0530

    Fixed response structure

--- server/src/Response.js
+++ server/src/Response.js
@@ -1,24 +1,31 @@
 function Response(data, status) {
-  this.status = status || 'success';
+  this.status = status || Response.TYPES.SUCCESS;
   this.data = data;
+  this.code = data.code || this.status === Response.TYPES.ERROR ? 500 : 200;
 }
-
+Response.TYPES = {
+  ERROR: 'error',
+  SUCCESS: 'success'
+};
 Response.prototype.json = function () {
-  var dataProp = this.status === 'error' ? 'error' : 'data';
+  var dataProp = this.status === Response.TYPES.ERROR ? Response.TYPES.ERROR : 'data';
   return {
     status: this.status,
+    statusCode: this.code,
     [dataProp]: this.data
   };
 };
 
-Response.prototype.success = function() {
-  this.status = 'success';
+Response.prototype.success = function () {
+  this.status = Response.TYPES.SUCCESS;
+  this.code = this.data.code || 200;
   return this.json();
-}
+};
 
-Response.prototype.error = function() {
-  this.status = 'error';
+Response.prototype.error = function () {
+  this.status = Response.TYPES.ERROR;
+  this.code = this.data.code || 500;
   return this.json();
-}
+};
 
 module.exports = Response;
