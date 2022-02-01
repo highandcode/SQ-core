6c654fd66d4d4607429e1e6209317a7153b98559
Navneet Nagpal <navneetnagpal84@gmail.com>
2022-02-01 16:52:22 +0530

    Fixed response key

--- server/src/Response.js
+++ server/src/Response.js
@@ -1,7 +1,7 @@
 function Response(data, status) {
   this.status = status || Response.TYPES.SUCCESS;
   this.data = data;
-  this.code = data.code || this.status === Response.TYPES.ERROR ? 500 : 200;
+  this.code = data.code || (this.status === Response.TYPES.ERROR ? 500 : 200);
 }
 Response.TYPES = {
   ERROR: 'error',
@@ -18,13 +18,13 @@ Response.prototype.json = function () {
 
 Response.prototype.success = function () {
   this.status = Response.TYPES.SUCCESS;
-  this.code = this.data.code || 200;
+  this.code = this.data.code || this.code || 200;
   return this.json();
 };
 
 Response.prototype.error = function () {
   this.status = Response.TYPES.ERROR;
-  this.code = this.data.code || 500;
+  this.code = this.data.code || this.code || 500;
   return this.json();
 };
 
