74ea224c17cfda7c23c029e533cce6a66f912a44
Navneet Nagpal <navneetnagpal84@gmail.com>
2022-07-15 10:44:42 +0000

    V2@next

--- jest.config.js
+++ jest.config.js
@@ -2,46 +2,37 @@
 // https://jestjs.io/docs/en/configuration.html
 
 module.exports = {
-  // Automatically clear mock calls and instances between every test
-  clearMocks: true,
-
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['web/**/*.{js,jsx,mjs}', 'vanillajs/**/*.{js,jsx,mjs}'],
-
-  // The directory where Jest should output its coverage files
-  coverageDirectory: 'coverage/ui',
-
-  // An array of file extensions your modules use
-  moduleFileExtensions: ['js', 'json', 'jsx'],
-
-  // The paths to modules that run some code to configure or set up the testing environment before each test
-  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
-  coverageReporters: ['text', 'clover', 'json', 'lcov'],
-  moduleNameMapper: {
-    '.+\\.(svg|png|jpg)$': 'identity-obj-proxy',
-    '^.+\\.(css|less|scss)$': 'babel-jest',
-  },
-  reporters: [
-    'default',
-    [
-      'jest-junit',
-      { outputDirectory: 'coverage/ui', outputName: 'junit.xml' },
-    ],
+  roots: ['<rootDir>'],
+  collectCoverageFrom: [
+    'web/**/*.{js,jsx,ts,tsx}',
+    '!web/**/*.d.ts',
+    'vanillajs/**/*.{js,jsx,ts,tsx}',
+    '!vanillajs/**/*.d.ts',
   ],
-  // The test environment that will be used for testing
-  testEnvironment: 'jest-environment-jsdom',
-
-  // The glob patterns Jest uses to detect test files
-  testMatch: ['**/__tests__/**/*.js?(x)', '**/(*.)(test).js?(x)'],
-
-  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: ['\\\\node_modules\\\\'],
-
-  // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
-
-  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: ['<rootDir>/node_modules/'],
-
-  // Indicates whether each individual test should be reported during the run
-  verbose: false,
+  setupFiles: ['react-app-polyfill/jsdom'],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  testMatch: [
+    '<rootDir>/web/**/__tests__/**/*.{js,jsx,ts,tsx}', 
+    '<rootDir>/web/**/*.{spec,test}.{js,jsx,ts,tsx}',
+    '<rootDir>/vanillajs/**/*.{spec,test}.{js,jsx,ts,tsx}',
+    '<rootDir>/vanillajs/**/*.{spec,test}.{js,jsx,ts,tsx}',
+  ],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': '<rootDir>/config/jest/babelTransform.js',
+    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+    '^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)': '<rootDir>/config/jest/fileTransform.js',
+  },
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  modulePaths: [],
+  moduleNameMapper: {
+    '^react-native$': 'react-native-web',
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
+  moduleFileExtensions: ['web.js', 'js', 'web.ts', 'ts', 'web.tsx', 'tsx', 'json', 'web.jsx', 'jsx', 'node'],
+  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+  resetMocks: true,
 };
