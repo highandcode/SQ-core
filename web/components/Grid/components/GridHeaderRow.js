b6ca03460551e5c4374c40d50cf9273d01e0fc4f
Nybble Core <nybblecore@gmail.com>
2022-09-12 12:34:58 +0530

    Added sort col name to override col name

--- web/components/Grid/components/GridHeaderRow.js
+++ web/components/Grid/components/GridHeaderRow.js
@@ -6,17 +6,18 @@ const GridHeaderRow = ({ columns = [], sortColumn, sortOrder = 'asc', onSort, en
   const isSortEnabled = enableSort && sortColumn && sortOrder;
   const sortOrderIconName = sortOrder?.toLowerCase() == "asc" ? 'arrowdropup' :  sortOrder?.toLowerCase() == "desc" ? 'arrowdropdown' : '';
   const handleClick = (e, column) => {
-    console.log(sortColumn, sortOrder, column.name);
+    const finalColName = column.sortColumnName || column.name;
     if (enableSort) {
       onSort && onSort({
-        sortColumn: column.name,
-        sortOrder: column.name === sortColumn ? sortOrder?.toLowerCase() === 'asc' ? 'desc' : 'asc' : 'asc'
+        sortColumn: finalColName,
+        sortOrder: finalColName === sortColumn ? sortOrder?.toLowerCase() === 'asc' ? 'desc' : 'asc' : 'asc'
       }, column);
     }
   }
   return (<div className={`sq-grid__row sq-grid__header-row ${enableSort ? 'sq-grid-header--sortable' : ''}`} role="header-row">
     {columns.map((column, index) => {
-      const hasSortColumn = column.name === sortColumn;
+      const finalColName = column.sortColumnName || column.name;
+      const hasSortColumn = finalColName === sortColumn;
       const isColSortEnabled = column.sort !== false;
       const hasEnabledSort = isColSortEnabled && isSortEnabled && hasSortColumn;
       return <div key={'gh-' + index} className={`sq-grid__cell sq-grid__header-row-cell ${isColSortEnabled ? 'sort-enable' : ''} ${column.className}`} onClick={(e)=> isColSortEnabled && handleClick(e, column)}>
