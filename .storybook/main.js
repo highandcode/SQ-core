8bb497daba650f55bda43eb7401ec91644e9e93e
Nybble Core <nybblecore@gmail.com>
2022-09-21 11:46:01 +0530

    Fixed storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,13 @@
-const babelConfig = require('../babel.config');
-// your app's webpack.config.js
+const babelConfig = require('../babel.config'); // your app's webpack.config.js
 
 const paths = require('../config/paths');
+
 const path = require('path');
 
 module.exports = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  babel: async (options) => ({
-    ...options,
-    ...babelConfig
-  }),
+  babel: async (options) => ({ ...options, ...babelConfig }),
   webpackFinal: (config) => {
     config.module.rules.push({
       test: /\.scss$/,
@@ -20,14 +17,28 @@ module.exports = {
         {
           loader: require.resolve('sass-loader'),
           options: {
-            data: ' @import "abstract/variables.scss";\n@import "mixins/index.scss";',
-            includePaths: [paths.appSrc, paths.styles, paths.assets],
-            sourceMap: true
-          }
-        }
+            sassOptions: {
+              includePaths: [paths.appSrc, paths.styles, paths.assets],
+            },
+            additionalData: (content, loaderContext) => {
+              if (loaderContext.resourcePath.endsWith('abstract\\_variables.scss') || loaderContext.resourcePath.endsWith('abstract/_variables.scss')) {
+                return content;
+              }
+              return `
+            @import "abstract/_variables.scss";
+            @import "mixins/index.scss";
+          ${content}
+        `;
+            },
+            sourceMap: true,
+          },
+        },
       ],
-      include: path.resolve(__dirname, '../')
+      include: path.resolve(__dirname, '../'),
     });
     return { ...config, module: { ...config.module } };
-  }
+  },
+  core: {
+    builder: 'webpack5',
+  },
 };
