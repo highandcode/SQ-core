207d5e289cd0469a2822dbc542a5db29ac864ccf
Nybble Core <nybblecore@gmail.com>
2022-10-18 11:19:38 +0530

    add default values

--- tests/ui/fake/content-actions.js
+++ tests/ui/fake/content-actions.js
@@ -6,7 +6,7 @@ export const create = ({
       pageData: {},
     },
   },
-}) => {
+} = {}) => {
   return {
     fetchContentPage: createAsyncAction(fetchContentPage),
     updateUserData: jest.fn(),
