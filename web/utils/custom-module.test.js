c4dcc11a8b9954f2f50f058ac2fe76cc2847a82f
Nybble Core <nybblecore@gmail.com>
2022-10-31 13:34:26 +0530

    added unit tests

--- web/utils/custom-module.test.js
+++ web/utils/custom-module.test.js
@@ -2,7 +2,7 @@ import CustomModule from './custom-module';
 
 describe('utils:CustomModule', () => {
   describe('CustomModule()', function () {
-   test('should have defined', async () => {
+    test('should have defined', async () => {
       expect(CustomModule).toBeDefined();
     });
   });
@@ -16,10 +16,10 @@ describe('utils:CustomModule', () => {
         },
       });
     });
-   test('should be able to add module', async () => {
+    test('should be able to add module', async () => {
       expect(module.modules.test).toBeDefined();
     });
-   test('should be able to add module', async () => {
+    test('should be able to add module', async () => {
       var result = await module.execute('test.addUser', 'test', 'in');
       expect(result).toEqual('test');
     });
@@ -36,26 +36,52 @@ describe('utils:CustomModule', () => {
         },
       });
     });
-   test('should be able to execute async module', async () => {
+    test('should be able to execute async module', async () => {
       var result = await module.execute('test.addUser', 'test', 'in');
       expect(result.test).toEqual(true);
       expect(result.p1).toEqual('test');
     });
   });
-  describe('If module don\'t exist Module', function () {
+  describe("If module don't exist Module", function () {
     let module;
     beforeEach(() => {
       module = new CustomModule();
-      // module.add('test', {
-      //   addUser: (response, action) => {
-      //     return new Promise((resolve) => {
-      //       setTimeout(() => resolve({ test: true, p1: response, p2: action }), 100);
-      //     });
-      //   },
-      // });
     });
-   test('should be able to execute async module', async () => {
-      expect(module.execute('test.addUser', 'test', 'in')).toThrow(true);
+    test('should throw an error', async () => {
+      let error;
+      await module.execute('test.addUser', 'test', 'in').catch((ex) => {
+        error = ex;
+      });
+      expect(error).toBe('test module not found');
+    });
+    test('should throw an error', async () => {
+      module.add('test', {
+        noName: (response, action) => {
+          return new Promise((resolve) => {
+            setTimeout(() => resolve({ test: true, p1: response, p2: action }), 100);
+          });
+        },
+      });
+      let error;
+      await module.execute('test.addUser', 'test', 'in').catch((ex) => {
+        error = ex;
+      });
+      expect(error).toBe('test.addUser action not found');
+    });
+    test('should throw an error', async () => {
+      module.add('test', {
+        noName: (response, action) => {
+          return new Promise((resolve) => {
+            setTimeout(() => resolve({ test: true, p1: response, p2: action }), 100);
+          });
+        },
+      });
+      let error;
+      module.remove('test')
+      await module.execute('test.addUser', 'test', 'in').catch((ex) => {
+        error = ex;
+      });
+      expect(error).toBe('test module not found');
     });
   });
 });
