0fefb498f32fe42cc56fcacd3c13174ee65f5890
“Nybble <nybbblecore@gmail.com>
2022-11-07 18:56:06 +0530

    Added sort text

--- web/components/Grid/components/GridHeaderRow.js
+++ web/components/Grid/components/GridHeaderRow.js
@@ -3,16 +3,18 @@ import PropTypes from 'prop-types';
 import { Tooltip } from '@mui/material';
 import Icon from '../../Icon';
 
-const GridHeaderRow = ({ columns = [], sortColumn, sortOrder = 'asc', onSort, enableSort, spacerWidth = 0 }) => {
+const GridHeaderRow = ({ columns = [], sortColumn, sortOrder = 'asc', sortSeparator = '|', onSort, enableSort, spacerWidth = 0 }) => {
   const isSortEnabled = enableSort && sortColumn && sortOrder;
   const handleClick = (e, column) => {
     const finalColName = column.sortColumnName || column.name;
     if (enableSort) {
+      const eSortOrder = finalColName === sortColumn ? (sortOrder?.toLowerCase() === 'asc' ? 'desc' : 'asc') : 'asc';
       onSort &&
         onSort(
           {
+            sortText: `${finalColName}${sortSeparator}${eSortOrder}`,
             sortColumn: finalColName,
-            sortOrder: finalColName === sortColumn ? (sortOrder?.toLowerCase() === 'asc' ? 'desc' : 'asc') : 'asc',
+            sortOrder: eSortOrder,
           },
           column
         );
@@ -23,23 +25,25 @@ const GridHeaderRow = ({ columns = [], sortColumn, sortOrder = 'asc', onSort, en
       {columns.map((column, index) => {
         const finalColName = column.sortColumnName || column.name;
         const hasSortColumn = finalColName === sortColumn;
-        const sortOrderIconName = hasSortColumn && sortOrder?.toLowerCase() == 'asc' ? 'sort-asc' : hasSortColumn && sortOrder?.toLowerCase() == 'desc' ? 'sort-desc' : 'sort-n';
+        const sortOrderIconName = hasSortColumn && sortOrder?.toLowerCase() === 'asc' ? 'sort-asc' : hasSortColumn && sortOrder?.toLowerCase() === 'desc' ? 'sort-desc' : 'sort-n';
         const isColSortEnabled = isSortEnabled && column.sort !== false;
         const hasSortApplied = isColSortEnabled && isSortEnabled && hasSortColumn;
         return (
           <div key={'gh-' + index} className={`sq-grid__cell sq-grid__header-row-cell ${hasSortApplied ? 'sort-active' : ''} ${isColSortEnabled ? 'sort-enable' : ''} ${column.className}`} onClick={(e) => isColSortEnabled && handleClick(e, column)}>
             <div className="sq-grid__header-row-cell-wrapper">
               <div className="sq-grid__cell sq-grid__header-row-cell-text">
-                {column.headerText && <Tooltip title={column.headerText}>
-                  <span>{column.headerText}</span>
-                </Tooltip>}
+                {column.headerText && (
+                  <Tooltip title={column.headerText}>
+                    <span>{column.headerText}</span>
+                  </Tooltip>
+                )}
               </div>
               {isColSortEnabled && <Icon variant={`${hasSortColumn ? 'black' : 'muted-light'}`} className="sq-grid__cell-sort" name={sortOrderIconName} />}
             </div>
           </div>
         );
       })}
-      {spacerWidth ? <div className='sq-grid__cell sq-grid__header-row-cell sq-grid__header-row__spacer' style={{flexBasis: `${spacerWidth}px`, flexGrow: 0, flexShrink: 0}}></div> : undefined}
+      {spacerWidth ? <div className="sq-grid__cell sq-grid__header-row-cell sq-grid__header-row__spacer" style={{ flexBasis: `${spacerWidth}px`, flexGrow: 0, flexShrink: 0 }}></div> : undefined}
     </div>
   );
 };
