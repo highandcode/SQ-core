0792e518cebd08afd2c48ebcef2437cda67edf67
Nybble Core <nybblecore@gmail.com>
2022-12-09 18:49:00 +0530

    Fixed icon button

--- web/components/ui/IconButton/index.js
+++ web/components/ui/IconButton/index.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
 import { redirectTo } from '../../../utils/redirect';
 import Icon from '../../Icon';
 
-const SQIconButton = ({ to = '', disabled, children, color = 'primary', className = '', onClick, iconSize = 'small', iconName, urlParams, ...rest }) => {
+const SQIconButton = ({ to = '', title, disabled, children, color = 'primary', className = '', onClick, iconSize = 'small', iconName, urlParams, ...rest }) => {
   const finalColor = disabled ? 'default' : color;
+  let button = (
+    <IconButton
+      className={className}
+      disabled={disabled}
+      onClick={(e) => {
+        !disabled && onClick && onClick(e);
+        !disabled && to && redirectTo(to, urlParams, { ...rest });
+      }}
+    >
+      <Icon name={iconName} variant={finalColor} color={finalColor} size={iconSize} />
+    </IconButton>
+  );
   return (
     <div className={`sq-icon-button ${disabled ? 'sq-icon-button--disabled' : ''}`}>
-      <IconButton
-        className={className}
-        disabled={disabled}
-        onClick={(e) => {
-          !disabled && onClick && onClick(e);
-          !disabled && to && redirectTo(to, urlParams, { ...rest });
-        }}
-      >
-        <Icon name={iconName} variant={finalColor} color={finalColor} size={iconSize} />
-      </IconButton>
+      {!title && {button}}
+      {title && (
+        <Tooltip title={title}>
+          <span>
+            {button}
+          </span>
+        </Tooltip>
+      )}
     </div>
   );
 };
