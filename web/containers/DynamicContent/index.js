0c1b9e1edad15e5956a47b872830f121e983f19f
“Nybble <nybbblecore@gmail.com>
2023-02-11 10:54:37 +0530

    Added pagebg

--- web/containers/DynamicContent/index.js
+++ web/containers/DynamicContent/index.js
@@ -525,9 +525,12 @@ class DynamicContent extends Component {
     const { out: tranOut = 'out-up', in: tranIn = 'out-in', loading = 'loading' } = transition;
     const classState = this.state.isOut ? `transition transition-page--${tranOut}` : this.state.isIn ? `transition transition-page--${tranIn}` : '';
     const loadingState = this.state.isLoading ? `transition transition-page--${loading}` : '';
-
+    const styles = {};
+    if (updatedPageData.pageBackground) {
+      styles.background = updatedPageData.pageBackground;
+    }
     return (
-      <div className={`dynamic-content ${rootClass} ${rootClassName} ${loadingState} ${classes.root || ''}`}>
+      <div className={`dynamic-content ${rootClass} ${rootClassName} ${loadingState} ${classes.root || ''}`} style={styles}>
         <Snackbar
           open={store.common.notification.show}
           message={store.common.notification.message}
