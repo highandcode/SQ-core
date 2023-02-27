9309d7101a41f69146c29229906673fef456be97
Nybble Core <nybblecore@gmail.com>
2023-02-27 16:23:28 +0530

    Fixed sticky comp

--- web/components/ui/GlobalNavigation/useSticky.js
+++ web/components/ui/GlobalNavigation/useSticky.js
@@ -1,44 +1,33 @@
 import { useEffect, useState, useRef } from 'react';
-
+let timer;
 function useSticky() {
   const [isSticky, setSticky] = useState(false);
   const element = useRef(null);
 
   const handleScroll = () => {
-    setTimeout(() => {
+    if (timer) {
+      clearTimeout(timer);
+      timer = null;
+    }
+    timer = setTimeout(() => {
+      timer = null;
       if (element.current) {
-        window.scrollY > element.current.getBoundingClientRect().top + element.current.getBoundingClientRect().height
-          ? setSticky(true)
-          : setSticky(false);
+        const val =
+          element.current.getBoundingClientRect().top +
+          element.current.getBoundingClientRect().height;
+        window.scrollY > val ? setSticky(true) : setSticky(false);
       }
-    }, 100);
+    }, 300);
   };
 
-  // This function handles the scroll performance issue
-  const debounce = (func, wait = 200, immediate = true) => {
-    let timeOut;
-    return () => {
-      let context = this,
-        args = arguments;
-      const later = () => {
-        timeOut = null;
-        if (!immediate) func.apply(context, args);
-      };
-      const callNow = immediate && !timeOut;
-      clearTimeout(timeOut);
-      timeOut = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
-    };
-  };
-
-  const deboundScroll = debounce(handleScroll);
+  const deboundScroll = handleScroll;
 
   useEffect(() => {
     window.addEventListener('scroll', deboundScroll);
     return () => {
       window.removeEventListener('scroll', deboundScroll);
     };
-  }, [debounce, handleScroll]);
+  }, [handleScroll]);
 
   return { isSticky, element };
 }
