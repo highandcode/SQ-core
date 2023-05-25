ab29ff5ca5635107126a559e6c6a356b7a5f90ae
Nybble Core <nybblecore@gmail.com>
2023-05-25 15:56:59 +0530

    Added class name for default

--- web/components/Dialog/index.js
+++ web/components/Dialog/index.js
@@ -31,7 +31,7 @@ const transitionOptions = {
   bottom: TransitionBottom,
 };
 
-const SQDialog = ({ closeButton = true, hideBackdrop = false, backDropClick = true, disableEscapeKeyDown, headerTag, transitionDir = 'up', classes: overrideClasses = {}, fullScreen = false, open = false, isLoading = false, title, content, children, onClose, actions = [], onAction }) => {
+const SQDialog = ({ className = '', closeButton = true, hideBackdrop = false, backDropClick = true, disableEscapeKeyDown, headerTag, transitionDir = 'up', classes: overrideClasses = {}, fullScreen = false, open = false, isLoading = false, title, content, children, onClose, actions = [], onAction }) => {
   console.log(overrideClasses.dialog?.root);
   const handleClose = (e, reason) => {
     if (reason === 'backdropClick' && backDropClick === false) {
@@ -51,7 +51,7 @@ const SQDialog = ({ closeButton = true, hideBackdrop = false, backDropClick = tr
   };
   return (
     <div className={`sq-dialog ${overrideClasses.root || ''}`}>
-      <Dialog hideBackdrop={hideBackdrop} disableEscapeKeyDown={disableEscapeKeyDown} TransitionComponent={transitionOptions[transitionDir]} classes={overrideClasses.dialog} open={open} fullScreen={fullScreen} onClose={handleClose}>
+      <Dialog hideBackdrop={hideBackdrop} disableEscapeKeyDown={disableEscapeKeyDown} TransitionComponent={transitionOptions[transitionDir]} classes={{root: className, ...overrideClasses.dialog}} open={open} fullScreen={fullScreen} onClose={handleClose}>
         <DialogTitle>
           {headerTag && <Typography variant={headerTag}>{title}</Typography>}
           {!headerTag && title}
